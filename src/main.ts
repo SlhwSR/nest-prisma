8ff3bf1819a146ce3c9473ec0a151050c2f2a9fb
hwlvd <lvhongwei02@gmail.com>
2023-01-06 14:29:01 +0800

    asf1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { Auth } from './auth/decorator/auth.decoator';
 import { AuthGuard } from '@nestjs/passport';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -11,6 +11,9 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe())
   // app.useGlobalPipes(new Validate());
+  app.setGlobalPrefix('api')
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
   await app.listen(3000); 
-}
+} 
 bootstrap();
+ 
