b8ea262f51326676d26cede295eb24c01f03a0de
吕泓炜 <lvhongwei0317@163.com>
2023-01-14 20:36:26 +0800

    ca1t

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,12 +11,29 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UploadModule } from './upload/upload.module';
 import { CategoryModule } from './category/category.module';
 import { VideoModule } from './video/video.module';
-
+import * as redisStores from 'cache-manager-redis-store';
 @Module({
-  imports: [ArticleModule, AuthModule,ConfigModule.forRoot({
-    load:[],
-    isGlobal:true
-  }), UserModule, PrismaModule, UploadModule, CategoryModule, VideoModule],
+  imports: [
+    ArticleModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      load: [],
+      isGlobal: true,
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      host: 'localhost',
+      port: 6379,
+      // store: redisStores,
+    }),
+    // Cache
+
+    UserModule,
+    PrismaModule,
+    UploadModule,
+    CategoryModule,
+    VideoModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
